47c177bee58b0136d3bfd6da58701406e3d78533
Javier <javierbarrezueta1@gmail.com>
2025-04-15 09:00:38 -0500

    Metodo Busqueda

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,8 +1,49 @@
 package controllers;
 
+import models.Person;
+import views.ShowConsole;
+
 public class MetodosBusqueda {
-    private views.ShowConsole ShowConsole;
-    private int[] arreglo;
-    public busquedalineal(int[] arreglo) {
+    private ShowConsole showConsole;
+    private Person[] people;
+    
+    public MetodosBusqueda(Person[] persons) {
+        showConsole = new ShowConsole();
+        this.people = persons;
+        showPersonByCode();
     }
+
+
+    public static int busquedaLineal (int[] arreglo, int busquedaLineal) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == busquedaLineal) {
+                System.out.println("El elemento " + busquedaLineal + " se encuentra en la posicion " + i);
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int findPersonByCode(int code){
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].getCode() == code) {
+                return i;
+            }
+        }
+        return -1;        
+    } 
+
+    public void showPersonByCode(){
+        int codetoFind = showConsole.getCode();
+        int index = findPersonByCode(codetoFind);
+        if (index >= 0) {
+            showConsole.showMessage("Persona encontrada: ");
+            showConsole.showMessage(people[index].toString()); 
+        } else {
+            showConsole.showMessage("Persona no encontrada");
+        }
+    
+    }
+
+
+
 }
--- src/views/ShowConsole.java
+++ src/views/ShowConsole.java
@@ -1,5 +1,22 @@
 package views;
 
+import java.util.Scanner;   
+
 public class ShowConsole {
-    
+    private Scanner scanner = new Scanner(System.in);
+
+    public void showMenu(){
+        System.out.println("Metodos de busqueda");
+    }
+
+    public int getCode(){
+        System.out.println("Ingrese el codigo");
+        int codigo = scanner.nextInt();
+        System.out.println("El codigo ingresado es: " + codigo);
+        return codigo;
+    }
+
+    public void showMessage(String message){
+        System.out.println(message);
+    }
 }
