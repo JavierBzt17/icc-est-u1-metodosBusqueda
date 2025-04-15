17711f65134385251694a357d60380de98f11119
Javier <javierbarrezueta1@gmail.com>
2025-04-15 07:41:58 -0500

    Ejercicio

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        private controllers.MetodosBusqueda metodosBusqueda
+    }
+    public main(String[] args){}
+}
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,8 @@
+package controllers;
+
+public class MetodosBusqueda {
+    private views.ShowConsole ShowConsole;
+    private int[] arreglo;
+    public busquedalineal(int[] arreglo) {
+    }
+}
